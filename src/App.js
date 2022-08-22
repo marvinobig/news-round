7f82d87384a335f32721cb1b1fd7e5fd9bed9318
Marvin Obig <88102362+marvinobig@users.noreply.github.com>
2022-08-22 13:05:03 +0100

    create user context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserContext from "./contexts/UserContext";
+import NavBar from "./components/NavBar/NavBar";
+import ArticlesPage from "./pages/ArticlesPage";
+
 function App() {
+  const [currUser, setCurrUser] = useState({});
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <BrowserRouter>
+      <UserContext.Provider value={{ currUser, setCurrUser }}>
+        <div className="App">
+          <header className="App-header">
+            <NavBar />
+          </header>
+
+          <Routes>
+            <Route path="/" element={<ArticlesPage />} />
+          </Routes>
+        </div>
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 }
 
