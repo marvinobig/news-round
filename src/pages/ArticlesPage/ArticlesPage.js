a1a6017ca52b4d77c65f92153208c9bdc478bb16
Marvin Obig <88102362+marvinobig@users.noreply.github.com>
2022-08-22 17:04:28 +0100

    import topicCard component

--- src/pages/ArticlesPage/ArticlesPage.js
+++ src/pages/ArticlesPage/ArticlesPage.js
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from "react";
 import ArticlesCard from "../../components/ArticlesCard/ArticlesCard";
+import TopicsCard from "../../components/TopicsCard/TopicsCard";
 import { fetchArticles } from "../../data/apiCalls";
 import styles from "./ArticlesPage.module.css";
 
-const ArticlesPage = () => {
+const ArticlesPage = ({ currTopic, setCurrTopic }) => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
     async function articlesData() {
-      const fetchedArticles = await fetchArticles();
-
+      const fetchedArticles = await fetchArticles(currTopic);
       setArticles((currArticles) => (currArticles = fetchedArticles));
     }
 
     articlesData();
-  }, []);
+  }, [currTopic]);
   return (
     <main className={styles.articles}>
+      <div>
+        <TopicsCard setCurrTopic={setCurrTopic} />
+      </div>
       <ArticlesCard articles={articles} />
     </main>
   );
