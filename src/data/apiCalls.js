16813e1257d2fbd01f24c724006f0376b24bbdc8
Marvin Obig <88102362+marvinobig@users.noreply.github.com>
2022-08-23 11:50:09 +0100

    create function to fetch single article

--- src/data/apiCalls.js
+++ src/data/apiCalls.js
@@ -13,3 +13,21 @@ exports.fetchTopics = async () => {
 
   return topicsJson.topics;
 };
+
+exports.fetchArticleById = async (article_id) => {
+  const article = await fetch(
+    `https://news-round-api.herokuapp.com/api/articles/${article_id}`
+  );
+  const articleJson = await article.json();
+
+  return articleJson.article;
+};
+
+exports.fetchArticleComments = async (article_id) => {
+  const articleComments = await fetch(
+    `https://news-round-api.herokuapp.com/api/articles/${article_id}/comments`
+  );
+  const articleCommentsJson = await articleComments.json();
+
+  return articleCommentsJson.articleComments;
+};
