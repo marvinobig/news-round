5ba77497af6f570b3404e5d6a0eb362032c1f285
Marvin Obig <88102362+marvinobig@users.noreply.github.com>
2022-08-23 10:05:36 +0100

    add error handling for incorrect topic

--- src/components/ArticlesCard/ArticlesCard.js
+++ src/components/ArticlesCard/ArticlesCard.js
@@ -11,36 +11,41 @@ import {
 const ArticlesCard = ({ articles }) => {
   return (
     <>
-      {articles.map((article) => {
-        return (
-          <div key={article.article_id} className={styles.article_card}>
-            <div className={styles.meta_info1}>
-              <p className={styles.user}>
-                {article.author[0].toUpperCase() + article.author.slice(1)}
-              </p>
-              <p className={styles.topic}>
-                {article.topic[0].toUpperCase() + article.topic.slice(1)}
-              </p>
-            </div>
-            <h2 className={styles.article_title}>{article.title}</h2>
-            <div className={styles.meta_info2}>
-              <p className={styles.date}>
-                Posted {moment(article.created_at).format("MMMM Do YYYY")}
-              </p>
-
-              <div className={styles.meta_info2_performance}>
-                <p>
-                  <FontAwesomeIcon icon={solid("comments")} />{" "}
-                  {article.comment_count}
+      {articles ? (
+        articles.map((article) => {
+          return (
+            <div key={article.article_id} className={styles.article_card}>
+              <div className={styles.meta_info1}>
+                <p className={styles.user}>
+                  {article.author[0].toUpperCase() + article.author.slice(1)}
                 </p>
-                <p>
-                  <FontAwesomeIcon icon={solid("thumbs-up")} /> {article.votes}
+                <p className={styles.topic}>
+                  {article.topic[0].toUpperCase() + article.topic.slice(1)}
                 </p>
               </div>
+              <h2 className={styles.article_title}>{article.title}</h2>
+              <div className={styles.meta_info2}>
+                <p className={styles.date}>
+                  Posted {moment(article.created_at).format("MMMM Do YYYY")}
+                </p>
+
+                <div className={styles.meta_info2_performance}>
+                  <p>
+                    <FontAwesomeIcon icon={solid("comments")} />{" "}
+                    {article.comment_count}
+                  </p>
+                  <p>
+                    <FontAwesomeIcon icon={solid("thumbs-up")} />{" "}
+                    {article.votes}
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })
+      ) : (
+        <p>No Such Topic Exists</p>
+      )}
     </>
   );
 };
