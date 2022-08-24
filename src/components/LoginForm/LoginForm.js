1fa50109d2a49966f9676fcb370bd0aab644963b
Marvin Obig <88102362+marvinobig@users.noreply.github.com>
2022-08-24 14:49:34 +0100

    create login component

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,83 @@
+import styles from "./LoginForm.module.css";
+import React, { useContext, useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import UserContext from "../../contexts/UserContext";
+import { fetchUsers } from "../../data/apiCalls";
+
+const LoginForm = () => {
+  const [users, setUsers] = useState([]);
+  const { currUser, setCurrUser } = useContext(UserContext);
+  const [loginInput, setLoginInput] = useState("");
+
+  useEffect(() => {
+    async function usersData() {
+      const fetchedUsers = await fetchUsers();
+      setUsers(fetchedUsers);
+    }
+
+    usersData();
+  }, []);
+
+  function showForm() {
+    const loginForm = document.querySelector(`#addLoginForm`);
+    loginForm.showModal();
+  }
+
+  function addLogin() {
+    const loginForm = document.querySelector(`#addLoginForm`);
+    const loggedInUser = users.filter((user) => user.username === loginInput);
+
+    setCurrUser(...loggedInUser);
+    loginForm.close();
+  }
+
+  function closeForm() {
+    const loginForm = document.querySelector(`#addLoginForm`);
+    loginForm.close();
+  }
+
+  return (
+    <>
+      <button className={styles.addLogin_btn} onClick={showForm}>
+        Login
+      </button>
+      <dialog id="addLoginForm" className={styles.addLogin_form}>
+        <form>
+          <div className={styles.close_container}>
+            <p>Login</p>
+            <button
+              type="button"
+              className={styles.close_btn}
+              onClick={closeForm}
+            >
+              <FontAwesomeIcon icon={solid("xmark")} />
+            </button>
+          </div>
+
+          <label htmlFor="userLogin">
+            Username
+            <input
+              id="userLogin"
+              type="text"
+              value={loginInput}
+              onChange={(e) => setLoginInput(e.target.value)}
+            />
+          </label>
+
+          <div className={styles.btn_container}>
+            <button
+              type="button"
+              onClick={addLogin}
+              className={styles.post_btn}
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </dialog>
+    </>
+  );
+};
+
+export default LoginForm;
