422f36f9ee397f25aabaf7bca2c32b27b7db77d5
Marvin Obig <88102362+marvinobig@users.noreply.github.com>
2022-08-24 14:50:45 +0100

    add login render logic

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,22 +3,19 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import UserContext from "../../contexts/UserContext";
 import ArticlePostForm from "../ArticlePostForm/ArticlePostForm";
+import LoginForm from "../LoginForm/LoginForm";
+import CurrUser from "../CurrUser/CurrUser";
 
 const NavBar = () => {
   const { currUser } = useContext(UserContext);
+
   return (
     <nav className={styles.nav}>
       <Link to="/">
         <button className={styles.nav_buttons}>Articles</button>
       </Link>
-      <h1>News Round</h1>
-      {currUser === {} ? (
-        <ArticlePostForm />
-      ) : (
-        <Link to="users">
-          <button className={styles.nav_buttons}>Login</button>
-        </Link>
-      )}
+      <CurrUser />
+      {currUser.username ? <ArticlePostForm /> : <LoginForm />}
     </nav>
   );
 };
