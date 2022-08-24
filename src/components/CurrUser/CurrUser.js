327e35698ee28fbc82f7b8b489d181b709375d42
Marvin Obig <88102362+marvinobig@users.noreply.github.com>
2022-08-24 14:50:02 +0100

    create current user component

---
+++ src/components/CurrUser/CurrUser.js
@@ -0,0 +1,62 @@
+import styles from "./CurrUser.module.css";
+import React, { useContext } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import UserContext from "../../contexts/UserContext";
+
+const CurrUser = () => {
+  const { currUser, setCurrUser } = useContext(UserContext);
+
+  function showForm() {
+    const currUserPopUp = document.querySelector(`#currUser`);
+    currUserPopUp.showModal();
+  }
+
+  function closeForm() {
+    const currUserPopUp = document.querySelector(`#currUser`);
+    currUserPopUp.close();
+  }
+
+  function logout() {
+    const currUserPopUp = document.querySelector(`#currUser`);
+    setCurrUser({});
+    currUserPopUp.close();
+  }
+
+  return (
+    <>
+      {currUser.username ? (
+        <button className={styles.currUser_btn} onClick={showForm}>
+          {currUser.username}
+        </button>
+      ) : (
+        <h1>NewsRound</h1>
+      )}
+
+      <dialog id="currUser" className={styles.currUser}>
+        <div className={styles.close_container}>
+          <p>Current User</p>
+          <button
+            type="button"
+            className={styles.close_btn}
+            onClick={closeForm}
+          >
+            <FontAwesomeIcon icon={solid("xmark")} />
+          </button>
+        </div>
+        <p>{currUser.name}</p>
+        <p>{currUser.username}</p>
+        <img
+          src={currUser.avatar_url}
+          alt={`${currUser.username} Profile Pic`}
+        />
+
+        <button className={styles.logout_btn} onClick={logout}>
+          Logout
+        </button>
+      </dialog>
+    </>
+  );
+};
+
+export default CurrUser;
