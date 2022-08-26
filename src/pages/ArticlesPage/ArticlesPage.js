ed8675dcb0cd5146cda7507c93189c907b57abb8
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-08-26 12:27:33 +0100

    add sort & order queries

--- src/pages/ArticlesPage/ArticlesPage.js
+++ src/pages/ArticlesPage/ArticlesPage.js
@@ -12,7 +12,9 @@ const ArticlesPage = ({ articleData }) => {
   useEffect(() => {
     async function articlesData() {
       const filter = searchParams.get("topic");
-      const fetchedArticles = await fetchArticles(filter || "");
+      const sort = searchParams.get("sort_by");
+      const order = searchParams.get("order");
+      const fetchedArticles = await fetchArticles(filter, sort, order);
 
       setArticles(fetchedArticles);
     }
