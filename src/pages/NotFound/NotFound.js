54dd405ab293f0f7ab520a13b9a8ae334b490f87
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-08-26 16:38:56 +0100

    add more styling to 404 page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <main>
+    <main className={styles.notFound}>
       <p>
         <span className={styles.error}>404</span>
       </p>
